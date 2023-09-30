83da76cc70d5c60cad6556ed9e87d5d44b8e7114
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-09-30 16:06:46 +0530

    Update leapyearnew.py

--- 30-09-23/leapyearnew.py
+++ 30-09-23/leapyearnew.py
@@ -1,12 +1,5 @@
 cyear=int(input("enter the current year:"))
 fyear=int(input("enter final year:"))
-if cyear < fyear:
-    print("here is the list of leap years between ",cyear,"and",fyear,"is:")
-while cyear< fyear:
-    if (cyear%4==0) & (cyear%100!=0):
-           print(cyear)
-    if (cyear%100==0) & (cyear %400==0):
-           print(cyear)
-    cyear=cyear+1
-if(cyear>=fyear):
-        print("check your year input angain.")
+for i in range(cyear,fyear+1):
+    if (i%4==0) & (i%100!=0) | (i%400==0):
+        print(i)
