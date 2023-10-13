a840363533dacbd78b5c8218945c8fb81d90df98
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-10-13 16:05:42 +0530

    Delete sumandavg.py

--- sumandavg.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter the total number you want enter:"))
-sum=0
-for i in range (n):
-    x=int(input("enter the number:"))
-    sum=sum+x
-print("sum=",sum)
-avg=sum/n
-print("average=",avg)
