020d77fef439c85f407dd14b7682605a670a7ea8
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-10-27 15:53:39 +0530

    Add files via upload

---
+++ 19-10-23/excolor.py
@@ -0,0 +1,8 @@
+list=[]
+n=int(input("enter number of elemets:"))
+for i in range(0,n):
+    ele=input("enter color:")
+    list.append(ele)
+    print(list)
+print("the first and last elemets are:",list[0],list[-1])    
+    
