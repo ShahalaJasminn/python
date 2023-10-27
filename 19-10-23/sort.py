ab02ed4e113f89a32b2ad7ce433d7e3382389421
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-10-27 15:41:16 +0530

    Add files via upload

---
+++ 19-10-23/sort.py
@@ -0,0 +1,6 @@
+d={1:2,3:4,4:3,2:1,0:0,3:5}
+print("orininal dictionary",d)
+sorted_d=sorted(d.items())
+print("dictionary in asending order:",sorted_d)
+sorted_d_reverse=sorted(d.items(),reverse=True)
+print("dictionary in desending order:",sorted_d_reverse)
