08aa1e6f35dc79c23f5c14ccc3325328a19a8b38
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-11-16 14:29:29 +0530

    Add files via upload

---
+++ 09-11-23/pkg.py
@@ -0,0 +1,10 @@
+from package import sum as sum1
+from package import pwr as pwr1
+from package import avg as avg1
+
+a=sum1.sum(10,20)
+b=avg1.average(10,20)
+c=pwr1.power(2,3)
+print(a)
+print(b)
+print(c)
