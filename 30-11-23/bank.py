956360ab87438ad741916aafc23ce38daa91f119
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-12-14 12:12:53 +0530

    Add files via upload

--- 30-11-23/bank.py
+++ 30-11-23/bank.py
@@ -9,7 +9,8 @@ class Bank_Account:
     def deposit(self):
          amount=float(input("Enter amount to be Deposited: "))
          self.balance += amount
-         print("Amount Deposited:",amount)
+         print("Amount Deposited:",amount)
+
 
     def withdraw(self):
         amount=(float(input("enter amount to be withdrawn:")))
