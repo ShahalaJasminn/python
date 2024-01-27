6707aded7487a3bd19c57837d8cc8b99e2ccb49f
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2024-01-26 19:43:21 -0800

    Update lambda.py

--- 26-10-23/lambda.py
+++ 26-10-23/lambda.py
@@ -9,5 +9,5 @@ print("Area of the rectangle is",area(a,b))
 
 a=int(input("Enter the height of the triangle \n"))
 b=int(input("Enter the breadth of the triangle \n"))
-area=lambda h,b:h*b
+area=lambda h,b:1/2*h*b
 print("Area of triangle is",area(a,b))
